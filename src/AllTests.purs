d13eb65325d037910c061ad52133626bbf9726a6
Gary Burgess <gary.burgess@gmail.com>
2014-04-25 22:02:58 +0100

    Add tests from purescript-either

--- src/AllTests.purs
+++ src/AllTests.purs
@@ -1,6 +1,15 @@
 module AllTests where
 
 import Control.Monad.Eff
+import Debug.Trace
 
 main = do
+  trace ""
+  trace ":: Data.Maybe ::"
+  trace ""
   Test.Data.Maybe.main
+  
+  trace ""
+  trace ":: Data.Either ::"
+  trace ""
+  Test.Data.Either.main

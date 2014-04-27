a492830fc693b5fdeed3d2e76f4bfeb5d21f8a91
Phil Freeman <paf31@cantab.net>
2014-04-27 14:55:05 -0700

    More regex tests

--- src/Test/Data/String/Regex.purs
+++ src/Test/Data/String/Regex.purs
@@ -17,3 +17,11 @@ main = do
   trace "test 'test' reports false for a mismatch"
   assert $ not $ test r "foo"
 
+  trace "test 'replaceR' replaces all matches"
+  assert $ replaceR r "work" "testing testing 1 2 3" == "working working 1 2 3" 
+
+  trace "test 'search' finds the first match"
+  assert $ search r "the string 'test' first appears in this test at index 12" == 12
+
+  trace "test 'match' reports the correct number of matches"
+  assert $ match r "test test test" == ["test", "test", "test"]
